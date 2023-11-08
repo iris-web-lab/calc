e322e44d0992db3a63de8f32fe06baddcfb5b477
iris-web-lab <139769654+iris-web-lab@users.noreply.github.com>
2023-11-09 00:44:27 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+    let display = document.getElementById("display");
 
+    function appendDisplay(char) {
+      if (display.textContent === "0") {
+        display.textContent = char;
+      } else {
+        display.textContent += char;
+      }
+    }
+
+    function clearDisplay() {
+      display.textContent = "0";
+    }
+
+    function evaluateDisplay() {
+      try {
+        let result = eval(display.textContent);
+        display.textContent = result;
+      } catch (error) {
+        display.textContent = "Error";
+      }
+    }
