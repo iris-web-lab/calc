d44b23e012e26fe2bf77aaa49ed6802393145a3e
iris-web-lab <139769654+iris-web-lab@users.noreply.github.com>
2023-11-09 02:22:24 +0100

    Update script.js

--- script.js
+++ script.js
@@ -8,6 +8,17 @@
       }
     }
 
+    Window.onload = function {
+   if ("serviceWorker" in navigator) {
+  window.addEventListener("load", function() {
+    navigator.serviceWorker
+      .register("/serviceWorker.js")
+      .then(res => console.log("service worker registered"))
+      .catch(err => console.log("service worker not registered", err))
+  })
+   }
+    }
+
     function clearDisplay() {
       display.textContent = "0";
     }
