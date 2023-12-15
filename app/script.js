bdaacf03c4636efbe68c5abdb38ffba88c04de99
Luka Đorđević <lukadordevi@gmail.com>
2023-12-15 17:25:45 +0100

    pwa

--- app/script.js
+++ app/script.js
@@ -1,4 +1,4 @@
-navigator.serviceWorker && navigator.serviceWorker.register('./sw.js').then(function (registration) {
+navigator.serviceWorker && navigator.serviceWorker.register('./../sw.js').then(function (registration) {
   console.log('Excellent, registered with scope: ', registration.scope);
 });
 
