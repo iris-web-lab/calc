91dac00f9afab0147a01fe0aabe9c2c51b97a5e2
Luka Đorđević <lukadordevi@gmail.com>
2023-12-22 19:54:22 +0100

    added offline functionality

--- sw.js
+++ sw.js
@@ -1,7 +1,27 @@
-/** An empty service worker! */
+// Choose a cache name
+const cacheName = 'cache-v1';
+// List the files to precache
+const precacheResources = ['/', '/favicon.ico', '/icon-192.png', '/icon-512-maskable.png', '/app/index.html', '/app/script.js', 'app/style.css', 'app/assets/OpenSans.ttf'];
 
-self.addEventListener('fetch', function (event) {
+// When the service worker is installing, open the cache and add the precache resources to it
+self.addEventListener('install', (event) => {
+  console.log('Service worker install event!');
+  event.waitUntil(caches.open(cacheName).then((cache) => cache.addAll(precacheResources)));
+});
 
-    /** An empty fetch handler! */
+self.addEventListener('activate', (event) => {
+  console.log('Service worker activate event!');
+});
 
+// When there's an incoming fetch request, try and respond with a precached resource, otherwise fall back to the network
+self.addEventListener('fetch', (event) => {
+  console.log('Fetch intercepted for:', event.request.url);
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+      return fetch(event.request);
+    }),
+  );
 });
