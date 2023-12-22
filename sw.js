2831a7fe4e7c4a1f52847b9970495b83213f6ccf
Luka Đorđević <lukadordevi@gmail.com>
2023-12-22 20:15:36 +0100

    fhgdfdgh

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // Choose a cache name
 const cacheName = 'cache-v1';
 // List the files to precache
-const precacheResources = ['/', '/app', '/app/index.html', '/app/script.js', 'app/style.css', 'app/assets/OpenSans.ttf'];
+const precacheResources = ['/', '/app/', '/app/assets', '/app/index.html', '/app/script.js', 'app/style.css', 'app/assets/OpenSans.ttf'];
 
 // When the service worker is installing, open the cache and add the precache resources to it
 self.addEventListener('install', (event) => {
