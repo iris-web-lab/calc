1996e44046cec8d302dfdb78636896ba42ec5b8b
Luka Đorđević <lukadordevi@gmail.com>
2023-12-23 20:33:48 +0100

    fgh

--- sw.js
+++ sw.js
@@ -11,7 +11,6 @@ const precacheResources = [
   'app/assets/OpenSans.ttf',
   '/favicon.ico',
   '/manifest.json',
-  ''
 ];
 
 // When the service worker is installing, open the cache and add the precache resources to it
