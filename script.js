6956c70d35adf2b646d30cbf5465e2a5897ab177
samolukadjo <82143316+samolukadjo@users.noreply.github.com>
2024-01-19 01:32:05 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-navigator.serviceWorker && navigator.serviceWorker.register('/sw.js?ver=14').then(function (registration) {
+navigator.serviceWorker && navigator.serviceWorker.register('/sw.js?ver=15').then(function (registration) {
   console.log('Excellent, registered with scope: ', registration.scope);
 });
 
@@ -8,7 +8,7 @@ const buttonClear = document.getElementById("button-clear");
 const buttonEqual = document.getElementById("button-equal");
 let errSetting = 0;
 let counter = 0;
-const version = 14;
+const version = 15;
 
 buttons.forEach(item => {
   let val = item.dataset.purpose;
@@ -74,7 +74,7 @@ function checkForUpdates() {
   console.log("Checking for updates 1/2");
   if ('serviceWorker' in navigator) {
     console.log("Checking for updates 2/2");
-    navigator.serviceWorker.register('/sw.js?ver=13').then((registration) => {
+    navigator.serviceWorker.register('/sw.js?ver=' + version).then((registration) => {
       const storedVersion = localStorage.getItem('sw-version');
       if (storedVersion !== version) {
         console.log("Installing updates.");
